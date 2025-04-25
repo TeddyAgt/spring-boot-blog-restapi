16f66136049c1115759df1b1da7b6676f2d8f8d0
Teddy Aboim <teddy.aboim@gmail.com>
2025-04-25 09:48:36 +0200

    initial commit

---
+++ src/test/java/fr/calmdown_teddy/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.calmdown_teddy.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
